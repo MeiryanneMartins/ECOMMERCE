3df99c01ef33f3c122e1f827c33d822413b8cab6
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2023-09-25 12:45:59 -0300

    Product model creation initiated with django

---
+++ product/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.conf import settings
+import os
+from PIL import Image
+from django.db import models
+from django.utils.text import slugify
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    short_description = models.TextField(max_length=255)
+    long_description = models.TextField()
+    image = models.ImageField(
+        upload_to= 'product_image/%Y/%m/', blank=True, null=True)
+    slug = models.SlugField(unique=True)
+    price_marketing = models.FloatField()
+    price_marketing_promotional = models.FloatField(default=0)
+    type = models.CharField(
+        default='V',
+        max_length=1,
+        choices=(
+            ('V', 'Variação'),
+            ('S', 'Simples'),
+        )
+    )
+    
+    @staticmethod
+    def resize_image(img, new_width=800):
+        img_full_path = os.path.join(settings.MEDIA_ROOT, img.name)
+        img_pil = Image.open(img_full_path)
+        original_width, original_height = img_pil.size
+
+        if original_width <= new_width:
+            img_pil.close()
+            return
+
+        new_height = round((new_width * original_height) / original_width)
+
+        new_img = img_pil.resize((new_width, new_height), Image.LANCZOS)
+        new_img.save(
+            img_full_path,
+            optimize=True,
+            quality=50
+        )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = f'{slugify(self.name)}'
+            self.slug = slug
+
+        super().save(*args, **kwargs)
+
+        max_image_size = 800
+
+        if self.image:
+            self.resize_image(self.image, max_image_size)
+
+    def __str__(self):
+        return self.nome
+    
+    def __str__(self):
+        return self.name
