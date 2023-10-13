2d502f4fa81565b866065f68009948057f137b59
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2023-10-13 19:55:11 -0300

    finish class addCart

--- product/views.py
+++ product/views.py
@@ -23,6 +23,9 @@ class DetailProduct(DetailView):
 
 class AddCart(View):
     def get(self, *args, **kwargs):
+        if self.request.session.get('car'):
+            del self.request.session['car']
+            self.request.session.save()
         http_referer = self.request.META.get(
             'HHTP_REFERER',
             reverse('product:list')
@@ -37,6 +40,30 @@ class AddCart(View):
             return redirect(http_referer)
 
         variation = get_object_or_404(models.Variation, id=variation_id)
+        product = variation.product
+        variation_stock = variation.stock
+
+        product_id = product.id
+        product_name = product.name
+        variation_name = variation.name or ''
+        price_marketing_promotional = variation.price_marketing_promotional
+        price = variation.price
+        amount = 1
+        slug = product.slug
+        image = product.image
+
+        if image:
+            image = image.name
+        else:
+            image = ''
+
+        if variation.stock < 1:
+            messages.error(
+                self.request,
+                'Estoque insuficiente'
+            )
+
+            return redirect(http_referer)
 
         if not self.request.session.get('car'):
             self.request.session['car'] = {}
@@ -45,12 +72,47 @@ class AddCart(View):
             car = self.request.session['car']
 
             if variation_id in car:
-                # TODO:variação existe no carrinho
+                amount_car = car[variation_id]['amount']
+                amount_car += 1
 
-                pass
+                if variation_stock < amount_car:
+                    messages.warning(
+                        self.request,
+                        f'Estoque insuficiente para {amount_car}x no'
+                        f'produto "{product.name}". Adicionamos {variation_stock}x'
+                        f'no seu carrinho.'
+                    )
+
+                    amount_car = variation_stock
+
+                    car[variation_id]['amount'] = amount_car
+                    car[variation_id]['price'] = price *\
+                        amount_car
+                    car[variation_id]['price_marketing_promotional'] = price_marketing_promotional *\
+                        amount_car
+
             else:
-                # TODO:variação não existe no carrinho
-                pass
+                car[variation_id] = {
+                    'product_id': product_id,
+                    'product_name': product_name,
+                    'variation_name': variation_name,
+                    'variation_id': variation_id,
+                    'price': price,
+                    'price_marketing_promotional': price_marketing_promotional,
+                    'amount': amount,
+                    'slug': slug,
+                    'image': image,
+                }
+
+                self.request.session.save()
+
+                messages.success(
+                    self.request,
+                    f'Produto{product_name} {variation_name} adicionado ao seu'
+                    f'carrinho {car[variation_id]["amount"]}x.'
+                )
+
+                return redirect(http_referer)
 
         return HttpResponse(f'{variation.product} {variation.name}')
 
