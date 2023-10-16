b548366f79d1ab067307982c54631398260c0b3b
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2023-10-16 15:21:17 -0300

    add filter for total in cart

--- product/templatetags/omfilters.py
+++ product/templatetags/omfilters.py
@@ -7,3 +7,13 @@ register = Library()
 @register.filter
 def format_price(val):
     return utils.format_price(val)
+
+
+@register.filter
+def cart_total_qtd(car):
+    return utils.cart_total_qtd(car)
+
+
+@register.filter
+def cart_totals(car):
+    return utils.cart_totals(car)
--- utils/utils.py
+++ utils/utils.py
@@ -1,2 +1,18 @@
 def format_price(val):
     return f'$ {val:.2f}'.replace('.', ',')
+
+
+def cart_total_qtd(car):
+    return sum([item['amount'] for item in car.values()])
+
+
+def cart_totals(car):
+    return sum(
+        [
+            item.get('price_amount_promotional')
+            if item.get('price_amount_promotional')
+            else item.get('price_amount')
+            for item
+            in car.values()
+        ]
+    )
