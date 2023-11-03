f1b20e2d5d989070f1f4dfb22737e465fa326930
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2023-11-03 17:40:23 -0300

    add variation check

--- order/views.py
+++ order/views.py
@@ -4,11 +4,42 @@ from django.views import View
 from django.http import HttpResponse
 from django.contrib import messages
 
+from product.models import Variation
+
 
 class Pay(View):
     template_name = 'order/pay.html'
 
     def get(self, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            messages.error(
+                self.request,
+                'Você precisa fazer login.'
+            )
+
+            return redirect('profile_user:criar')
+
+        if not self.request.session.get('car'):
+            messages.error(
+                self.request,
+                'Carrinho vazio.'
+            )
+
+            return redirect('product:list')
+
+        car = self.request.session.get('car')
+        car_variations_ids = [v for v in car]
+        bd_variations = list(
+            Variation.objects.select_related(
+                'product').filter(id__in=car_variations_ids)
+        )
+
+        for variation in bd_variations:
+            vid = variation.id
+
+            stock = variation.stock
+            qtd_car = car[vid]['amount']
+
         contexto = {
 
         }
